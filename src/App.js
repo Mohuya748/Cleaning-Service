87350396eb8ee894d4a4cdc4deead9b24cc8915a
Mohuya748 <khandakermohuya@gmail.com>
2022-08-20 12:27:24 +0600

    nav done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
+import Navbar from "./Components/Navbar/Navbar";
+import { Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import About from "./Pages/About";
+import Services from "./Pages/Services";
+import Login from "./Pages/Login";
+import Contact from "./Pages/Contact";
 
 
 function App() {
   return (
     <div>
-    <h1>Cleaning service</h1>
+   <Navbar>
+    <Routes>
+      <Route path = '/' element={<Home></Home>}></Route>
+      <Route path = '/about' element={<About></About>}></Route>
+      <Route path = '/services' element={<Services></Services>}></Route>
+      <Route path = '/login' element={<Login></Login>}></Route>
+      <Route path = '/contact' element={<Contact></Contact>}></Route>
+    </Routes>
+   
+   </Navbar>
+   
     </div>
   );
 }
